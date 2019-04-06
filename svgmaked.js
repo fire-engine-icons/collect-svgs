b7a6b8bd91e9f64888ff895a9d00aa93b508c632
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2019-04-06 20:14:30 +0200

    Delete svgmaked.js

--- svgmaked.js
+++
@@ -1,8 +0,0 @@
-var page = require('webpage').create();
-console.log("COLLECTING");
-page.open('http://gif.sharkbyte.bplaced.net/SVG/', function() {
-    setTimeout(function() {
-        page.render('svgs.png');
-        phantom.exit();
-    }, 3000);
-});
